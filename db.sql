6360500f593cf5f66c8afe8e653bafe05adfebfd
Tienluc037 <nguyentienluc12a3@gmail.com>
2022-02-17 09:08:47 +0700

    case

---
+++ db.sql
@@ -0,0 +1,18 @@
+create database `caseinote` collate utf8_unicode_ci;
+
+use caseinote;
+
+create table note_type (
+                           id int(11) primary key auto_increment,
+                           name varchar(255),
+                           description varchar(255)
+);
+
+
+create table note(
+                     id int(11) primary key auto_increment,
+                     title varchar(255),
+                     content varchar(500),
+                     type_id int,
+                     foreign key (type_id) references note_type(id)
+);
